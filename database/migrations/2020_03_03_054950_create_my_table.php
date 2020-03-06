6b887885e16a872afdbd06163b8e3a7e50f63c9b
mgarcia21 <201413473@ingenieria.usac.edu.gt>
2020-03-05 19:01:29 -0600

    Login y base de datos

---
+++ database/migrations/2020_03_03_054950_create_my_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMyTable extends Migration {
+
+  /**
+   * Run the migrations.
+   *
+   * @return void
+   */
+  public function up()
+  {
+      Schema::create('users', function(Blueprint $table)
+      {
+          $table->increments('id');
+
+          $table->string('name', 32);
+          $table->string('username', 32);
+          $table->string('email', 320);
+          $table->string('password', 64);
+
+                      // required for Laravel 4.1.26
+                      $table->string('remember_token', 100)->nullable();
+          $table->timestamps();
+      });
+  }
+
+  /**
+   * Reverse the migrations.
+   *
+   * @return void
+   */
+  public function down()
+  {
+      Schema::drop('users');
+  }
+
+}
