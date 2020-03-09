5234017fbc26b7e4670f2430ffc00c0cb655f696
mgarcia21 <201413473@ingenieria.usac.edu.gt>
2020-03-08 23:54:04 -0600

    Dashboard

---
+++ resources/views/dashboard/dashboard.blade.php
@@ -0,0 +1,5 @@
+@extends('dashboard.layout')
+
+@section('content')
+<h1>Sertek</h1>
+@endsection('content')
--- routes/web.php
+++ routes/web.php
@@ -34,3 +34,7 @@ Route::get('/home', 'HomeController@index')->name('home');
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route::get('admin', function(){
+	return view('dashboard.dashboard');
+});
